5bcc2d30b7d7e35d7fa322eb57979199b4f1d006
FrancoBG79 <fbg2468@gmail.com>
2026-04-07 12:53:57 +0200

    section_2_video_20

--- blog-boilerplate/client/src/App.js
+++ blog-boilerplate/client/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import PostCreate from "./Postcreate";
 
 const App = () => {
-  return <div>Blog App</div>;
+  return <div className="container">
+    <h1>Create Post</h1>
+    <PostCreate />
+  </div>;
 };
 
 export default App;
