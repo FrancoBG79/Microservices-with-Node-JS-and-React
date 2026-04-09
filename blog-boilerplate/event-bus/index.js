61ed8d7cf57fbe0047889fd37d64317bb1ed5db1
FrancoBG79 <fbg2468@gmail.com>
2026-04-09 10:12:48 +0200

    section_2_video_32

---
+++ blog-boilerplate/event-bus/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('', (req, res) => {
+  res.send('Hello Event Bus');
+});
+
+app.post('/events', (req, res) => {
+  res.send('Hello Event Bus');
+  const event = req.body;
+
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log('Error occurred while posting to posts service:', err.message);
+  });
+
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log('Error occurred while posting to comments service:', err.message);
+  });
+
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log('Error occurred while posting to queries service:', err.message);
+  });
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('Listening on 4005');
+});
